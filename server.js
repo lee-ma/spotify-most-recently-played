1d34ab2316382403a419fb510fdaceba75327855
lee-ma <leema9321@gmail.com>
2019-06-14 18:31:11 -0700

    testing

--- server.js
+++ server.js
@@ -1,25 +1,67 @@
 var express = require('express');
 var axios = require('axios');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
 app.use(cors({
-  origin: "https://www.leema.ca"
+  origin: "http://localhost:8000"
 }));
 
 app.get("/", function(req,res) {
   res.send("Hello World!");
-})
+});
 
 app.get("/recentlyPlayed", function(req, res) {
+
+  var data = {};
+
   axios.get(process.env.LASTFM_URL)
   .then(function(response) {
-    res.send(response.data);
+    data = response.data.recenttracks.track;
+    res.send(data);
   }).catch(function(error) {
     console.log(error);
   })
+});
+
+
+app.get("/spotifyLink", function(req,res) {
+  var title = req.query.title;
+  var artist = req.query.artist;
+  var album = req.query.album;
+
+  var stringToEncode = process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET;
+
+  var auth = Buffer.from(stringToEncode).toString('base64');
+
+  const config = {
+    headers: {
+      'Authorization': `Basic ${auth}`,
+      'Content-Type': 'application/x-www-form-url-encoded'
+    }
+  }
+
+  const body = {
+    grant_type: 'client_credentials'
+  }
+
+  axios.post('https://accounts.spotify.com/api/token', body, config)
+  .then(function(response) {
+    res.send(response);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+  
+  // axios.get('https://api.spotify.com/v1/search?q=track:"' + title + '"%20artist:"' + artist + +'"%20album:"' + album + '"&type=track&limit=1')
+
 });
 
 app.listen(port, function() {
