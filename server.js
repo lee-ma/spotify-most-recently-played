94f7a7b14045c31bed222fc2c62b91f0286a1fa6
lee-ma <leema9321@gmail.com>
2019-06-15 00:46:45 -0700

    production

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: "http://localhost:8000"
+  origin: "http://www.leema.ca"
 }));
 
 app.get("/", function(req,res) {
