ca973945387ec158892e6b2459c38d683c385b8f
lee-ma <leema9321@gmail.com>
2019-06-15 08:56:48 -0700

    testing

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: "https://www.leema.ca"
+  origin: "http://localhost:8000"
 }));
 
 app.get("/", function(req,res) {
