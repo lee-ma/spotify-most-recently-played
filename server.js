170bb4e58792c6ff63c124639baac281214cdfcb
lee-ma <leema9321@gmail.com>
2019-06-16 23:19:47 -0700

    use env vars for cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: "https://www.leema.ca"
+  origin: process.env.CORS_URL
 }));
 
 app.get("/", function(req,res) {
